2d0181b5b41955de69a1d497e6ae52e21f95efb9
konrad101296 <konradslimak@o2.pl>
2024-02-28 19:49:40 +0100

    Add project files.

---
+++ week 2 lesson 7 exercise nr.4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("podaj rok: ");
+int rok = int.Parse(Console.ReadLine());
+
+int wynik = rok % 4;
+
+if (wynik == 0)
+{
+    Console.WriteLine($"{rok} jest rokiem przestepnym");
+}
+else
+{
+    Console.WriteLine($"{rok} nie jest rokiem przestepnym");
+}
