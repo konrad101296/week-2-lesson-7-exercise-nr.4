5a1e18709d075d5aa1883a6ddcb9d429e9afc82e
konrad101296 <161070317+konrad101296@users.noreply.github.com>
2024-03-05 20:04:59 +0100

    Update Program.cs
    dodano rok przestepny co 400 lat

--- week 2 lesson 7 exercise nr.4/Program.cs
+++ week 2 lesson 7 exercise nr.4/Program.cs
@@ -1,13 +1,18 @@
-﻿Console.WriteLine("podaj rok: ");
-int rok = int.Parse(Console.ReadLine());
+Console.WriteLine("podaj rok: ");
+int year = int.Parse(Console.ReadLine());
 
-int wynik = rok % 4;
-
-if (wynik == 0)
+int result = year % 4;
+int result2 = year % 100;
+int result3 = year % 400;
+if (result2 == 0 && result3 != 0)
 {
-    Console.WriteLine($"{rok} jest rokiem przestepnym");
+    Console.WriteLine($"{year} is not a leap year");
+}
+else if (result == 0)
+{
+    Console.WriteLine($"{year} is a leap year");
 }
 else
 {
-    Console.WriteLine($"{rok} nie jest rokiem przestepnym");
+    Console.WriteLine($"{year} is not a leap year");
 }
